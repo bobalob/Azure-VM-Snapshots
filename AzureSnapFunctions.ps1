a8324bfb188626d4dd581bbe926d51b45dc4cf60
bobalob <bobalob@gmail.com>
2016-10-23 17:24:36 +0100

    Add files via upload
    Reversion working

--- AzureSnapFunctions.ps1
+++ AzureSnapFunctions.ps1
@@ -1,6 +1,28 @@
-﻿# SnapshotVM.ps1
-# Create snapshot of the given Azure RM VM 
+﻿<#
+.SYNOPSIS
+    A set of functions to create, delete or revert to a snapshot of an Azure RM VM
+.DESCRIPTION
+    A detailed description of the function or script. This keyword can be
+    used only once in each topic.
+.NOTES
+    File Name      : AzureSnapFunctions.ps1
+    Author         : Dave Hall
+    Prerequisite   : PowerShell V5 (Tested with V5, may work in earlier)
+                     AzureRM Powershell Module (Install-Module AzureRM)
+    Copyright 2016 - Dave Hall
+.LINK
+    http://superautomation.blogspot.com
+.EXAMPLE
+    Example 1
+.EXAMPLE
+    Example 2
+#>
 
+#Import-Module AzureRM
+#Login-AzureRMAccount
+. "$($PSScriptRoot)\AzureStorageFunctions.PS1"
+
+#TODO: Add Name/Description for snaps
 Function Create-AzureRMVMSnap {
 	Param(
 		[Parameter(Mandatory=$true)]$VMName
@@ -9,62 +31,49 @@ Function Create-AzureRMVMSnap {
 	Write-Host "Create Snapshot for VM: " -ForegroundColor Yellow -NoNewline
 	Write-Host $VMName -ForegroundColor Cyan
 
-	if (!(Get-AzureAccount)) {
-		$AzureAccount = Add-AzureAccount
-	}
-
 	$VM = Get-AzureRmVM | ? {$_.Name -eq $VMName}
 	if ($VM) {
-		$DiskUriList=@()
+		$SnapshotGUID = New-GUID
+
+        $DiskUriList=@()
 		$DiskUriList += $VM.StorageProfile.OsDisk.Vhd.Uri
 		Foreach ($Disk in $VM.StorageProfile.DataDisks) {
 			$DiskUriList += $Disk.Vhd.Uri
 		}
-		
+
 		$Snapshots=@()
+        $SnapshotTableInfo=@()
 		Foreach ($DiskUri in $DiskUriList) {
 			Write-Host "Snapshot disk: " -ForegroundColor Yellow -NoNewline
 			Write-Host $DiskUri -ForegroundColor Cyan
-			
-			$DiskToSnap = "" | Select Uri, StorageAccountName, VHDName, ContainerName
-			$DiskToSnap.Uri = $DiskUri
-			$DiskToSnap.StorageAccountName = ($DiskUri -split "https://")[1].Split(".")[0]
-			$DiskToSnap.VHDName = $DiskUri.Split("/")[-1]
-			$DiskToSnap.ContainerName = $DiskUri.Split("/")[-2]
-				
-			$StorageAccountResource = find-azurermresource -ResourceNameContains `
-                $DiskToSnap.StorageAccountName
 
-			$StorageKey = Get-AzureRmStorageAccountKey `
-                -Name $StorageAccountResource.Name `
-                -ResourceGroupName $StorageAccountResource.ResourceGroupName
+	        $DiskInfo = Get-DiskInfo $DiskUri
 
-			$StorageContext = New-AzureStorageContext `
-                -StorageAccountName $StorageAccountResource.Name `
-                -StorageAccountKey $StorageKey[0].Value
+			$StorageContext = Get-StorageContextForUri -DiskUri $DiskUri
 
-			$DiskBlob = Get-AzureStorageBlob -Container $DiskToSnap.ContainerName `
-                -Context $StorageContext -Blob $DiskToSnap.VHDName
+			$DiskBlob = Get-AzureStorageBlob -Container $DiskInfo.ContainerName `
+                -Context $StorageContext -Blob $DiskInfo.VHDName
 
-			$Snapshots += $DiskBlob.ICloudBlob.CreateSnapshot()
+			$Snapshot = $DiskBlob.ICloudBlob.CreateSnapshot()
+            
+            Write-SnapInfo -VMName $VMName -SnapGUID $SnapshotGUID `
+                -PrimaryUri $DiskInfo.Uri.ToString() `
+                -SnapshotUri $Snapshot.SnapshotQualifiedStorageUri.PrimaryUri.AbsoluteUri.ToString() `
+                -StorageContext $StorageContext -DiskNum $DiskUriList.IndexOf($DiskUri)
 
 		}
-
-		$Snapshots
-
+        #TODO: This will only retrieve storage info for a single context 
+        #  so if disks are split over storage accounts it won't work properly
+        Retrieve-SnapInfo -VMName $VMName -SnapGUID $SnapshotGUID -StorageContext $StorageContext
 	} else {
 		Write-Host "Unable to get VM"
 	}}
-
-Function Get-AzureRMVMSnap {
+#TODO: Combine the Get functions
+Function Get-AzureRMVMSnapBlobs {
 	Param(
 		[Parameter(Mandatory=$true)]$VMName
 	)
 
-	if (!(Get-AzureAccount)) {
-		$AzureAccount = Add-AzureAccount
-	}
-
 	$VM = Get-AzureRmVM | ? {$_.Name -eq $VMName}
 	if ($VM) {
 		$DiskUriList=@()
@@ -72,35 +81,21 @@ Function Get-AzureRMVMSnap {
 		Foreach ($Disk in $VM.StorageProfile.DataDisks) {
 			$DiskUriList += $Disk.Vhd.Uri
 		}
-		
 		$Snapshots=@()
 		Foreach ($DiskUri in $DiskUriList) {
 			Write-Host "Snapshots for Disk: " -ForegroundColor Yellow -NoNewline
 			Write-Host $DiskUri -ForegroundColor Cyan
-			
-			$DiskToSnap = "" | Select Uri, StorageAccountName, VHDName, ContainerName
-			$DiskToSnap.Uri = $DiskUri
-			$DiskToSnap.StorageAccountName = ($DiskUri -split "https://")[1].Split(".")[0]
-			$DiskToSnap.VHDName = $DiskUri.Split("/")[-1]
-			$DiskToSnap.ContainerName = $DiskUri.Split("/")[-2]
-				
-			$StorageAccountResource = find-azurermresource `
-                -ResourceNameContains $DiskToSnap.StorageAccountName
 
-			$StorageKey = Get-AzureRmStorageAccountKey `
-                -Name $StorageAccountResource.Name `
-                -ResourceGroupName $StorageAccountResource.ResourceGroupName
+	        $DiskInfo = Get-DiskInfo -DiskUri $DiskUri
 
-			$StorageContext = New-AzureStorageContext `
-                -StorageAccountName $StorageAccountResource.Name `
-                -StorageAccountKey $StorageKey[0].Value
+			$StorageContext = Get-StorageContextForUri -DiskUri $DiskUri
 
-			Get-AzureStorageBlob -Container $DiskToSnap.ContainerName `
-                -Context $StorageContext | 
-                ? {$_.Name -eq $DiskToSnap.VHDName `
-                        -and $_.ICloudBlob.IsSnapshot `
-                        -and $_.SnapshotTime -ne $null
-            }
+                Get-AzureStorageBlob -Container $DiskInfo.ContainerName `
+                    -Context $StorageContext | 
+                    ? {$_.Name -eq $DiskInfo.VHDName `
+                            -and $_.ICloudBlob.IsSnapshot `
+                            -and $_.SnapshotTime -ne $null
+                    }
 		}
 
 	} else {
@@ -108,24 +103,128 @@ Function Get-AzureRMVMSnap {
 	}
 }
 
-Function Delete-AzureRMVMSnap {
+Function Get-AzureRMVMSnap {
+	Param(
+		[Parameter(Mandatory=$true)]$VMName,
+        $SnapshotGUID
+	)
+	$VM = Get-AzureRmVM | ? {$_.Name -eq $VMName}
+	if ($VM) {
+        $OSDiskUri += $VM.StorageProfile.OsDisk.Vhd.Uri
+        $StorageContext = Get-StorageContextForUri -DiskUri $OSDiskUri
+
+        if ($SnapshotGUID) {
+            $SnapInfo = Retrieve-SnapInfo -VMName $VMName -SnapGUID $SnapshotGUID -StorageContext $StorageContext
+        } else {
+            $SnapInfo = Retrieve-SnapInfo -VMName $VMName -StorageContext $StorageContext
+        }
+
+        $SnapshotBlobs = Get-AzureRMVMSnapBlobs -VMName $VMName
+        Foreach ($SnapInfo in $SnapInfo) {
+            $SnapshotBlobs | ? {$_.ICloudBlob.SnapshotQualifiedStorageUri.PrimaryUri.AbsoluteUri.ToString() -eq $SnapInfo.SnapshotUri.ToString()}
+        }
+
+    }
+
+
+}
+
+Function Delete-AzureRMVMSnapBlobs {
 	Param (
         [Parameter(Mandatory=$true)]$VMName, 
         [switch]$DeleteAll=$False
     )
 	
-    $DiskSnaps = Get-AzureRMVMSnap -VMName $VMName 
-
     if ($DeleteAll) {
+        $DiskSnaps = Get-AzureRMVMSnapBlobs -VMName $VMName
         $DiskSnaps | % {$_.ICloudBlob.Delete()}
     } else {
-        Foreach ($Snap in $DiskSnaps) {
-            $Snap
-            $Delete = Read-Host "Delete this snap? [y/N]: "
-            if ($Delete -eq "y") {
-                $Snap.ICloudBlob.Delete()
+	    $VM = Get-AzureRmVM | ? {$_.Name -eq $VMName}
+	    if ($VM) {
+            $OSDiskUri += $VM.StorageProfile.OsDisk.Vhd.Uri
+            $StorageContext = Get-StorageContextForUri $OSDiskUri
+            $SnapInfo = Retrieve-SnapInfo -VMName $VMName -StorageContext $StorageContext
+            $UniqueGuids = $SnapInfo | % {$_.SnapGuid} | Sort -Unique
+            Foreach ($Guid in $UniqueGuids) {
+                $SnapBlobs = Get-AzureRMVMSnap -VMName $VMName -SnapshotGUID $Guid
+                $SnapBlobs | Select -First 1
+                $Delete = Read-Host "$($SnapBlobs.Count) Disks in this snapshot set - Delete this snap? [y/N]: "
+                if ($Delete -eq "y") {
+                    $SnapBlobs | % {$_.ICloudBlob.Delete()}
+                }
+            }
+        } else {
+            Write-Host "Unable to find VM"
+        }
+    }   
+}
+
+Function Revert-AzureRMVMSnap {
+	Param(
+		[Parameter(Mandatory=$true)]$VMName
+	)
+	$VM = Get-AzureRmVM | ? {$_.Name -eq $VMName}
+	if ($VM) {
+        #Get user selected snapshot
+        #TODO: Accept snapshot GUID or Name as parameter
+        $OSDiskUri += $VM.StorageProfile.OsDisk.Vhd.Uri
+        $StorageContext = Get-StorageContextForUri $OSDiskUri
+        $SnapInfo = Retrieve-SnapInfo -VMName $VMName -StorageContext $StorageContext
+        $UniqueGuids = $SnapInfo | % {$_.SnapGuid} | Sort -Unique
+        Foreach ($Guid in $UniqueGuids) {
+            $SnapBlobs = Get-AzureRMVMSnap -VMName $VMName -SnapshotGUID $Guid
+            $SnapBlobs | Select -First 1
+            $Revert = Read-Host "$($SnapBlobs.Count) Disks in this snapshot set - Revert to this snap? [y/N]: "
+            if ($Revert -eq "y") {
+
+                #Shut down the VM
+                Write-Host "Stopping the VM..."
+                $VM | Stop-AzureRMVM -Force
+
+                #Delete the VM
+                Write-Host "Deleting the VM..."
+                $VM | Remove-AzureRmVm -Force
+
+                Foreach ($SnapBlob in $SnapBlobs) {
+                    $ThisSnap = $SnapInfo | ? {$_.SnapGUID -eq $guid -and $_.SnapshotUri -eq `
+                        $SnapBlob.ICloudBlob.SnapshotQualifiedStorageUri.PrimaryUri.AbsoluteUri.ToString()}
+                    Write-Host "Reverting disk $($ThisSnap.DiskNum)..."
+                    $DiskInfo = Get-DiskInfo -DiskUri $SnapBlob.ICloudBlob.Uri.OriginalString
+                    $OriginalDiskBlob = Get-AzureStorageBlob -Container $DiskInfo.ContainerName `
+                        -Context $StorageContext | 
+                        ? {$_.Name -eq $DiskInfo.VHDName `
+                                -and -not $_.ICloudBlob.IsSnapshot `
+                                -and $_.SnapshotTime -eq $null
+                        }
+                    $OriginalDiskBlob.ICloudBlob.StartCopyFromBlob($SnapBlob.ICloudBlob) 
+                    #TODO: Enter into loop until copy finished     
+                }
+
+                #Remove disallowed settings
+                Write-Host "Recreate the VM..."
+                $VM.StorageProfile.OsDisk.OsType = $null
+                $VM.StorageProfile.ImageReference = $Null
+                $VM.OSProfile = $null
+
+                #Old VM Information
+                $rgName=$VM.ResourceGroupName
+                $locName=$VM.Location
+                $osDiskUri=$VM.StorageProfile.OsDisk.Vhd.Uri
+                $diskName=$VM.StorageProfile.OsDisk.Name
+                $osDiskCaching = $VM.StorageProfile.OsDisk.Caching
+
+                #Set the OS disk to attach
+                $vm=Set-AzureRmVMOSDisk -VM $vm -VhdUri $osDiskUri -name $DiskName `
+                    -CreateOption attach -Windows -Caching $osDiskCaching
+
+                #Recreate the VM
+                New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $VM
+
+                #Break out of the loop
+                Break
             }
         }
+
+
     }
-    
 }
---
+++ AzureStorageFunctions.ps1
@@ -0,0 +1,122 @@
+﻿<#
+.SYNOPSIS
+    Helper functions for AzureSnapFunctions.ps1
+.NOTES
+    File Name      : AzureStorageFunctions.ps1
+    Author         : Dave Hall
+    Prerequisite   : PowerShell V5 (Tested, may work in earlier)
+                     AzureRM Powershell Module
+    Copyright 2016 - Dave Hall
+.LINK
+    http://superautomation.blogspot.com
+.EXAMPLE
+    Example 1
+.EXAMPLE
+    Example 2
+#>
+
+Function Get-StorageTable {
+  Param(
+    [Parameter(Mandatory=$true)]$TableName,
+    [Parameter(Mandatory=$true)]$StorageContext
+    )
+    $SnapTable = Get-AzureStorageTable -Context $StorageContext | 
+        ? {$_.CloudTable.Name -eq $TableName}
+    if (!($SnapTable)) {
+      $SnapTable = New-AzureStorageTable -Name $TableName -Context $StorageContext
+    }
+    return $SnapTable
+}
+
+Function Retrieve-SnapInfo {
+  Param(
+      $VMName,
+      $SnapGUID,
+      [Parameter(Mandatory=$true)]$StorageContext
+    )
+
+    $TableName = "AzureVMSnapshots"
+    $SnapTable = Get-StorageTable -TableName $TableName -StorageContext $StorageContext
+
+    $query = New-Object "Microsoft.WindowsAzure.Storage.Table.TableQuery"
+    if ($SnapGUID) {
+      $Query.FilterString = "PartitionKey eq '$($SnapGUID)'"
+    } 
+
+    $SnapInfo = $SnapTable.CloudTable.ExecuteQuery($query)
+    foreach ($snap in $SnapInfo) {
+      $OutputItem = "" | Select VMName, SnapshotName, DiskNum, SnapGUID, PrimaryUri, SnapshotUri, SnapshotDescription, SnapshotTime
+      $OutputItem.SnapGUID = $Snap.PartitionKey
+      $OutPutItem.DiskNum = $Snap.RowKey
+      $OutputItem.VMName = $Snap.Properties.VMName.StringValue
+      $OutputItem.PrimaryUri = $Snap.Properties.BaseURI.StringValue
+      $OutputItem.SnapshotUri = $Snap.Properties.SnapshotURI.StringValue
+      $OutputItem.SnapshotName = $Snap.Properties.SnapshotName.StringValue
+      $OutputItem.SnapshotDescription = $Snap.Properties.SnapshotDescription.StringValue
+      $OutputItem.SnapshotTime = $Snap.Properties.SnapshotTime.StringValue
+      if ($VMName) { 
+        $OutputItem | ? {$_.VMName -eq $VMName}
+      } else {
+        $OutputItem
+      }
+    }
+}
+
+Function Write-SnapInfo {
+  Param(
+    [Parameter(Mandatory=$true)]$VMName,
+    [Parameter(Mandatory=$true)]$DiskNum,
+    [Parameter(Mandatory=$true)]$SnapGUID,
+    [Parameter(Mandatory=$true)]$PrimaryUri,
+    [Parameter(Mandatory=$true)]$SnapshotUri,
+    [Parameter(Mandatory=$true)]$SnapshotName,
+    $SnapshotDescription="",
+    [Parameter(Mandatory=$true)]$StorageContext
+  )
+  $TableName = "AzureVMSnapshots"
+  $SnapTable = Get-StorageTable -TableName $TableName -StorageContext $StorageContext
+
+    $entity = New-Object "Microsoft.WindowsAzure.Storage.Table.DynamicTableEntity" `
+      $SnapGUID, $DiskNum
+
+    $entity.Properties.Add("VMName", $VMName)
+    $entity.Properties.Add("BaseURI", $PrimaryUri)
+    $entity.Properties.Add("SnapshotURI", $SnapshotUri)
+    $entity.Properties.Add("SnapshotName", $SnapshotName)
+    $entity.Properties.Add("SnapshotDescription", $SnapshotDescription)
+    $entity.Properties.Add("SnapshotTime", ((Get-Date).ToString()))
+    $result = $SnapTable.CloudTable.Execute([Microsoft.WindowsAzure.Storage.Table.TableOperation]::Insert($entity))
+}
+
+#TODO: Write Clear-SnapInfo($GUID) Function
+
+Function Get-DiskInfo {
+    Param([Parameter(Mandatory=$true)]$DiskUri) 
+    
+    $DiskInfo = "" | Select Uri, StorageAccountName, VHDName, ContainerName
+    $DiskInfo.Uri = $DiskUri
+    $DiskInfo.StorageAccountName = ($DiskUri -split "https://")[1].Split(".")[0]
+    $DiskInfo.VHDName = $DiskUri.Split("/")[-1]
+    $DiskInfo.ContainerName = $DiskUri.Split("/")[-2]
+    Return $DiskInfo		
+}
+
+Function Get-StorageContextForUri {
+    Param([Parameter(Mandatory=$true)]$DiskUri) 
+			
+	$DiskInfo = Get-DiskInfo -DiskUri $DiskUri
+				
+	$StorageAccountResource = find-azurermresource `
+        -ResourceNameContains $DiskInfo.StorageAccountName `
+         -WarningAction Ignore
+
+	$StorageKey = Get-AzureRmStorageAccountKey `
+        -Name $StorageAccountResource.Name `
+        -ResourceGroupName $StorageAccountResource.ResourceGroupName
+
+	$StorageContext = New-AzureStorageContext `
+        -StorageAccountName $StorageAccountResource.Name `
+        -StorageAccountKey $StorageKey[0].Value
+
+    return $StorageContext
+}
